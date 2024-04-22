09183d34a598fd0eed3aa8b065772316977a2f53
unknown <ponshriharini@gmail.com>
2024-04-22 10:30:06 +0530

    Reservation Model created

---
+++ Day9/SolHotelManagement/HotelManagementModels/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace HotelManagementModels
+{
+    internal class Reservation
+    {
+        public Reservation()
+        {
+            Id = 0;
+            Customer = 0;
+            TotalCost = 0;
+            CancellationPolicy = string.Empty;
+        }
+
+        public Reservation(int id, int customer, double totalCost, string cancellationPolicy)
+        {
+            Id = id;
+            Customer = customer;
+            TotalCost = totalCost;
+            CancellationPolicy = cancellationPolicy;
+        }
+
+        public int Id { get; set; }
+        public int Customer { get; set; }
+        public double TotalCost { get; set; }
+        public string CancellationPolicy { get; set; }
+
+    }
+}
+
+
