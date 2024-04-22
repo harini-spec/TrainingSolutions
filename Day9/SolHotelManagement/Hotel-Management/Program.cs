413adb42ef977bd9b840d1c1dddc23e9cbda8ae6
unknown <ponshriharini@gmail.com>
2024-04-22 10:36:26 +0530

    Folder structure created

---
+++ Day9/SolHotelManagement/Hotel-Management/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
