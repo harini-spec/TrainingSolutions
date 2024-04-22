ef78f1ab4d8ff93d41a65df01a95bf76eea2d749
unknown <ponshriharini@gmail.com>
2024-04-22 10:30:35 +0530

    Room Model created

---
+++ Day9/SolHotelManagement/HotelManagementModels/Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementModels
+{
+    internal class Room
+    {
+        public Room()
+        {
+            Id = 0;
+            RoomType = string.Empty;
+            Features = new List<string>();
+            OccupancyCapacity = 0;
+            NightlyRate = 0;
+        }
+
+        public Room(int id, string roomType, List<string> features, int occupancyCapacity, double nightlyRate)
+        {
+            Id = id;
+            RoomType = roomType;
+            Features = features;
+            OccupancyCapacity = occupancyCapacity;
+            NightlyRate = nightlyRate;
+        }
+
+        public int Id { get; set; }
+        public string RoomType {  get; set; }
+        public List<string> Features { get; set; }
+        public int OccupancyCapacity { get; set; }
+        public double NightlyRate { get; set; }
+
+
+    }
+}
