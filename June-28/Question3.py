8b47e3711f4d405c0a7d2cc0a64adcc6c9da0340
unknown <ponshriharini@gmail.com>
2024-07-01 09:11:41 +0530

    Added comments

--- June-28/Question3.py
+++ June-28/Question3.py
@@ -21,7 +21,7 @@ players_data = {"Ram": 100,
                 "Lanita": 1900, 
                 "Nanita": 2000}
 
-players_data = dict(sorted(players_data.items(), key=lambda x: x[1], reverse=True)) # reverse = True for descending order, lambda takes score values
+players_data = dict(sorted(players_data.items(), key=lambda x: x[1], reverse=True)) # reverse = True for descending order, lambda takes score values, sorted based on key value which is lambda value = score 
 print("Top 10 players are:")
 for i in range(0, 10):
     print(list(players_data.keys())[i], ":", list(players_data.values())[i]) # Takes all keys as list and gets data from index 
